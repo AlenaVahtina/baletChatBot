5e348889a511690a1cc618466831c412fedd60eb
avakhtina <https://mail.phoenixit.ru>
2019-02-10 01:37:38 +0300

    start bot

---
+++ bot.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+import vk_api
+
+# сообщество
+token = "token"
+vk_session = vk_api.VkApi(token=token)
+
+try:
+    vk_session.auth(token_only=True)
+except vk_api.AuthError as error_msg:
+    print(error_msg)
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+longpoll = VkLongPoll(vk_session)
+vk = vk_session.get_api()
+for event in longpoll.listen():
+    vk.messages.send(user_id=event.user_id,
+                     random_id=333,
+                     message='test',
+                     keyboard=open("question.json", "r").read())
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+        if event.text == 'Расписание':
+            print 'yes'
+            if event.from_user:
+                vk.messages.send(  # Отправляем сообщение
+                    user_id=event.user_id,
+                    random_id=event.random_id,
+                    message='Занимаемся Вт, Чт и Пт с 19:30. Занятия идут 1.5 часа')
+        if event.text == 'Адрес проведения занятийе':
+            print 'yes'
+            if event.from_user:
+                vk.messages.send(  # Отправляем сообщение
+                    user_id=event.user_id,
+                    random_id=event.random_id,
+                    message='Адрес бла-бла')
+        if event.text == 'Стоимость занятий':
+            print 'yes'
+            if event.from_user:
+                vk.messages.send(  # Отправляем сообщение
+                    user_id=event.user_id,
+                    random_id=event.random_id,
+                    message='Еще не решено')
+        if event.text == 'Контакты':
+            print 'yes'
+            if event.from_user:
+                vk.messages.send(  # Отправляем сообщение
+                    user_id=event.user_id,
+                    random_id=event.random_id,
+                    message='Вот прямо так и сказала')
