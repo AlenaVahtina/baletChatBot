e9bc8306af23c1749c4ba3bc3911a2e62b57e2da
avakhtina <https://mail.phoenixit.ru>
2019-02-16 21:03:33 +0300

    encoding

--- bot.py
+++ bot.py
@@ -14,33 +14,29 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
 for event in longpoll.listen():
-    vk.messages.send(user_id=event.user_id,
-                     random_id=333,
-                     message='test',
-                     keyboard=open("question.json", "r").read())
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-        if event.text == 'Расписание':
+        if event.text == u'Расписание':
             print 'yes'
             if event.from_user:
                 vk.messages.send(  # Отправляем сообщение
                     user_id=event.user_id,
                     random_id=event.random_id,
                     message='Занимаемся Вт, Чт и Пт с 19:30. Занятия идут 1.5 часа')
-        if event.text == 'Адрес проведения занятийе':
+        if event.text == u'Адрес проведения занятийе':
             print 'yes'
             if event.from_user:
                 vk.messages.send(  # Отправляем сообщение
                     user_id=event.user_id,
                     random_id=event.random_id,
                     message='Адрес бла-бла')
-        if event.text == 'Стоимость занятий':
+        if event.text == u'Стоимость занятий':
             print 'yes'
             if event.from_user:
                 vk.messages.send(  # Отправляем сообщение
                     user_id=event.user_id,
                     random_id=event.random_id,
                     message='Еще не решено')
-        if event.text == 'Контакты':
+        if event.text == u'Контакты':
             print 'yes'
             if event.from_user:
                 vk.messages.send(  # Отправляем сообщение
