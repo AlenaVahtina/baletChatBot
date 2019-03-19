359f23e0d5412fbbede56268433729c9484e43e3
avakhtina <https://mail.phoenixit.ru>
2019-03-20 01:29:33 +0300

    Start scheduler

--- baletStoryBot.py
+++ baletStoryBot.py
@@ -1,11 +1,19 @@
 # coding=utf-8
 from vk_api.upload import VkUpload
 from connect import *
+import  sched, time
 
 upload_url = None
+s = sched.scheduler(time.time, time.sleep)
 
-upload = vk_api.VkUpload(vk_session)
-upload.story(
-    'balteR3.png',
-    'photo',
-    group_id='178073199')
+
+def create_story(file_name='balteR3.png'):
+    upload = vk_api.VkUpload(vk_session)
+    upload.story(
+        file_name,
+        'photo',
+        group_id='178073199')
+
+def its_time_create_story():
+    s.enter(10, 1, create_story())
+    s.run()
